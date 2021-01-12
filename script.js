056d4a775baee3efb7f2ffa6a16aeb5dc07eec97
Chee Min Hao <65438002+CheeMinHao@users.noreply.github.com>
2021-01-12 10:50:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const addProfile = (ev) => {
 		bio: document.getElementById("bio").value
 	}
 	database.push(profile);
-	document.form[0].reset(); //to clear form for next entry
+	document.forms[0].reset(); //to clear form for next entry
 
 	localStorage.setItem('myProfile', JSON.stringify(database));
 	window.open("index(2).html");
