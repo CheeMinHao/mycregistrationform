fe3fb6d6246f8edf3f601d8281f61de212aad3aa
Chee Min Hao <65438002+CheeMinHao@users.noreply.github.com>
2021-01-12 11:07:29 +0800

    Update script(2).js

--- script(2).js
+++ script(2).js
@@ -1,8 +1,12 @@
 window.onload = function() {
-	var disProfile = localStorage.getItem('myProfile ');
-	var currentProfile = disProfile;
-	document.getElementById("profile_name").innerHTML = "Name: " + (currentProfile[0]["username"]);
-	document.getElementById("profile_age").innerHTML = "Age: " + (currentProfile[0]["age"]);
-	document.getElementById("profile_dob").innerHTML = "Date of Birth: " + (currentProfile[0]["dob"]);
-	document.getElementById("profile_bio").innerHTML = "Bio: " + (currentProfile[0]["bio"]);
+	var disProfile = localStorage.getItem('myProfile');
+	var currentProfile = JSON.parse(disProfile);
+	var res = [];
+	for(var i in currentProfile) {
+		res.push(currentProfile[i])
+	}
+	document.getElementById("profile_name").innerHTML = "Name: " + (res[0]["username"]);
+	document.getElementById("profile_age").innerHTML = "Age: " + (res[0]["age"]);
+	document.getElementById("profile_dob").innerHTML = "Date of Birth: " + (res[0]["dob"]);
+	document.getElementById("profile_bio").innerHTML = "Bio: " + (res[0]["bio"]);
 }
