f97dbf1305ca342542e316b2e5339e796dee8034
Chee Min Hao <65438002+CheeMinHao@users.noreply.github.com>
2021-01-12 10:20:53 +0800

    Add files via upload

---
+++ script(2).js
@@ -0,0 +1,9 @@
+var disProfile = localStorage.getItem('myProfile ');
+var currentProfile = disProfile;
+
+window.onload = function() {
+	document.getElementById("profile_name").innerHTML = "Name: " + (currentProfile[0]["username"]);
+	document.getElementById("profile_age").innerHTML = "Age: " + (currentProfile[0]["age"]);
+	document.getElementById("profile_dob").innerHTML = "Date of Birth: " + (currentProfile[0]["dob"]);
+	document.getElementById("profile_bio").innerHTML = "Bio: " + (currentProfile[0]["bio"]);
+}
---
+++ script.js
@@ -0,0 +1,27 @@
+let database = [];
+const addProfile = (ev) => {
+	ev.preventDefault();
+	let profile = {
+		username: document.getElementById("username").value,
+		age: document.getElementById("age").value,
+		dob: document.getElementById("dob").value,
+		bio: document.getElementById("bio").value
+	}
+	database.push(profile);
+	document.form-group[0].reset(); //to clear form for next entry
+
+	localStorage.setItem('myProfile', JSON.stringify(database));
+	window.open("index(2).html");
+}
+
+// function displayProfile() {
+// 	var profile = localStorage.getItem("myProfile");
+// 	document.getElementById("profile_name").innerHTML = "Name: " + profile[0]["username"];
+// 	document.getElementById("profile_age").innerHTML = "Age: " + profile[0]["age"];
+// 	document.getElementById("profile_dob").innerHTML = "Date of Birth: " + profile[0]["dob"];
+// 	document.getElementById("profile_bio").innerHTML = "Bio: " + profile[0]["bio"];
+// }
+
+
+
+document.getElementById('submit').addEventListener('click', addProfile);
