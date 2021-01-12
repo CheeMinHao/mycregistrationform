b20398a61445768c24a69b0500a723fca09e9a2e
Chee Min Hao <65438002+CheeMinHao@users.noreply.github.com>
2021-01-12 13:31:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let database = [];
 const addProfile = (ev) => {
 	ev.preventDefault();
-	var imageBefore = document.getElementById("img-label");
+// 	var imageBefore = document.getElementById("img-label");
 	let profile = {
 		username: document.getElementById("username").value,
 		age: document.getElementById("age").value,
 		dob: document.getElementById("dob").value,
 		bio: document.getElementById("bio").value,
-		image: getBase64Image(imageBefore)
+// 		image: getBase64Image(imageBefore)
 	}
 	database.push(profile);
 	document.querySelector('form-group').reset(); //to clear form for next entry
@@ -15,26 +15,17 @@ const addProfile = (ev) => {
 	localStorage.setItem('myProfile', JSON.stringify(database));
 	window.open("index(2).html");
 
-	function getBase64Image(img) {
-		var canvas = document.createElement("canvas");
-    	canvas.width = img.width;
-    	canvas.height = img.height;
+// 	function getBase64Image(img) {
+// 		var canvas = document.createElement("canvas");
+//     	canvas.width = img.width;
+//     	canvas.height = img.height;
 
-    	var ctx = canvas.getContext("2d");
-    	ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+//     	var ctx = canvas.getContext("2d");
+//     	ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
-    	return canvas.toDataURL("image/png");
-	}
+//     	return canvas.toDataURL("image/png");
+// 	}
 }
-
-
-// function displayProfile() {
-// 	var profile = localStorage.getItem("myProfile");
-// 	document.getElementById("profile_name").innerHTML = "Name: " + profile[0]["username"];
-// 	document.getElementById("profile_age").innerHTML = "Age: " + profile[0]["age"];
-// 	document.getElementById("profile_dob").innerHTML = "Date of Birth: " + profile[0]["dob"];
-// 	document.getElementById("profile_bio").innerHTML = "Bio: " + profile[0]["bio"];
-// }
 
 
 
