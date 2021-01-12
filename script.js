7a5ad029738ba1f63eb705f19224d97b1b3e6320
Chee Min Hao <65438002+CheeMinHao@users.noreply.github.com>
2021-01-12 11:39:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 let database = [];
 const addProfile = (ev) => {
 	ev.preventDefault();
+	var imageBefore = document.getElementById("img-label");
 	let profile = {
 		username: document.getElementById("username").value,
 		age: document.getElementById("age").value,
 		dob: document.getElementById("dob").value,
-		bio: document.getElementById("bio").value
+		bio: document.getElementById("bio").value,
+		image: getBase64Image(imageBefore)
 	}
 	database.push(profile);
-	document.forms[0].reset(); //to clear form for next entry
+	document.querySelector('form-group').reset(); //to clear form for next entry
 
 	localStorage.setItem('myProfile', JSON.stringify(database));
 	window.open("index(2).html");
+
+	function getBase64Image(img) {
+		var canvas = document.createElement("canvas");
+    	canvas.width = img.width;
+    	canvas.height = img.height;
+
+    	var ctx = canvas.getContext("2d");
+    	ctx.drawImage(img, 0, 0);
+
+    	var dataURL = canvas.toDataURL("image/png");
+
+    	return dataURL.replace(/^data:image\/(png|jpg);base64,/, "")
+	}
 }
 
 // function displayProfile() {
