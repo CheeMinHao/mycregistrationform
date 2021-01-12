39e37a18bf1155782a9d0f5ae08ac77c36acace7
Chee Min Hao <65438002+CheeMinHao@users.noreply.github.com>
2021-01-12 15:25:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 let database = [];
 const addProfile = (ev) => {
 	ev.preventDefault();
-// 	var imageBefore = document.getElementById("img-label");
 	let profile = {
 		username: document.getElementById("username").value,
 		age: document.getElementById("age").value,
 		dob: document.getElementById("dob").value,
 		bio: document.getElementById("bio").value,
-		image: document.getElementById("img").value
-// 		image: getBase64Image(imageBefore)
+		image: drawingImage();
 	}
 	database.push(profile);
-	document.forms[0].reset();; //to clear form for next entry
+	document.forms[0].reset(); //to clear form for next entry
 
 	localStorage.setItem('myProfile', JSON.stringify(database));
 	window.open("index(2).html");
 
-// 	function getBase64Image(img) {
-// 		var canvas = document.createElement("canvas");
-//     	canvas.width = img.width;
-//     	canvas.height = img.height;
+	function drawingImage() {
+		var photo = document.getElementById("img");
+		var imgCanvas = document.createElement("canvas");
+		imgContext = imgCanvas.getContext("2d");
 
-//     	var ctx = canvas.getContext("2d");
-//     	ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+		imgCanvas.width = img.width;
+		imgCanvas.height = img.height;
 
-//     	return canvas.toDataURL("image/png");
-// 	}
+		imgContext.drawImage(img, 0, 0, img.width, img.height);
+		var imgAsDataURL = imgCanvas.toDataURL("data:image/jpg;base64, ");
+		return imgAsDataURL;
+	}
 }
-
-
 
 document.getElementById('submit').addEventListener('click', addProfile);
