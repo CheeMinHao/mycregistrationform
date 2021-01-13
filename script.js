cae65e27c2fcd8dd29d762e3d982e1aca8da05a9
Chee Min Hao <65438002+CheeMinHao@users.noreply.github.com>
2021-01-13 11:34:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,26 +6,20 @@ const addProfile = (ev) => {
 		age: document.getElementById("age").value,
 		dob: document.getElementById("dob").value,
 		bio: document.getElementById("bio").value,
-		image: drawingImage(document.getElementById("img").value)
 	}
 	database.push(profile);
+
+	document.querySelector("#img").addEventListener("change", function() {
+		const reader = new FileReader();
+		localStorage.setItem("saved-img", reader.result);
+		// reader.readAsDataURL(this.files[0]);
+	});
+
 	document.forms[0].reset(); //to clear form for next entry
 
 	localStorage.setItem('myProfile', JSON.stringify(database));
 	window.open("index(2).html");
-
-	function drawingImage(img) {
-		var photo = document.getElementById("img");
-		var imgCanvas = document.createElement("canvas");
-		imgContext = imgCanvas.getContext("2d");
-
-		imgCanvas.width = img.width;
-		imgCanvas.height = img.height;
-
-		imgContext.drawImage(img, 0, 0, img.width, img.height);
-		var imgAsDataURL = imgCanvas.toDataURL("data:image/jpg;base64, ");
-		return imgAsDataURL;
-	}
 }
+
 
 document.getElementById('submit').addEventListener('click', addProfile);
