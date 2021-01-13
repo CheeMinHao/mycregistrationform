59164718ced6a8827126a3717c1b0288e277052d
Chee Min Hao <65438002+CheeMinHao@users.noreply.github.com>
2021-01-13 14:35:18 +0800

    Update script(2).js

--- script(2).js
+++ script(2).js
@@ -2,6 +2,7 @@ window.onload = function() {
 	var disProfile = localStorage.getItem('myProfile');
 	var currentProfile = JSON.parse(disProfile);
 	const recentImageDataURL = localStorage.getItem("saved_img");
+	const reader = new FileReader();
 	var res = [];
 	for(var i in currentProfile) {
 		res.push(currentProfile[i])
@@ -10,5 +11,5 @@ window.onload = function() {
 	document.getElementById("profile_age").innerHTML = "Age: " + (res[0]["age"]);
 	document.getElementById("profile_dob").innerHTML = "Date of Birth: " + (res[0]["dob"]);
 	document.getElementById("profile_bio").innerHTML = "Bio: " + (res[0]["bio"]);
-	document.getElementById("profile_img").src = recentImageDataURL;
+	document.getElementById("profile_img").src = reader.readAsDataURL(recentImageDataURL);
 }
